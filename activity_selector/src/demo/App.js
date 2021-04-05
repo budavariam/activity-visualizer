a000ec734d1d4ebf073dc3dcc76c41261b8d90c7
Mátyás Budavári <budavariam@gmail.com>
2021-04-05 10:30:49 +0200

    Improve look and feel

--- activity_selector/src/demo/App.js
+++ activity_selector/src/demo/App.js
@@ -1,52 +1,65 @@
 /* eslint no-magic-numbers: 0 */
 import React, { Component } from 'react';
+import "./App.css";
 
 import { ActivitySelector } from '../lib';
 
+const activities = [
+    { id: "0", name: "Warmup Stretch", start_date: "2020-03-14", has_heartrate: false },
+    { id: "1", name: "Run", start_date: "2020-03-14", has_heartrate: true, average_heartrate: 165, max_heartrate: 180 },
+    { id: "2", name: "Yoga", start_date: "2021-03-15", has_heartrate: true, average_heartrate: 60, max_heartrate: 90 },
+    { id: "3", name: "Workout", start_date: "2021-03-15", has_heartrate: true, average_heartrate: 145, max_heartrate: 172 },
+    { id: "4", name: "Workout", start_date: "2021-03-15", has_heartrate: true, average_heartrate: 145, max_heartrate: 172 },
+    { id: "5", name: "Workout", start_date: "2021-03-15", has_heartrate: true, average_heartrate: 145, max_heartrate: 172 },
+    { id: "6", name: "Workout", start_date: "2019-03-15", has_heartrate: true, average_heartrate: 145, max_heartrate: 172 },
+    { id: "7", name: "Workout", start_date: "2019-03-15", has_heartrate: true, average_heartrate: 145, max_heartrate: 172 },
+    { id: "8", name: "Workout", start_date: "2019-03-15", has_heartrate: true, average_heartrate: 145, max_heartrate: 172 },
+    { id: "9", name: "Workout", start_date: "2019-03-15", has_heartrate: true, average_heartrate: 145, max_heartrate: 172 },
+    { id: "10", name: "Workout", start_date: "2019-03-15", has_heartrate: true, average_heartrate: 145, max_heartrate: 172 },
+    { id: "11", name: "Workout", start_date: "2019-03-15", has_heartrate: true, average_heartrate: 145, max_heartrate: 172 },
+    { id: "12", name: "Workout", start_date: "2019-03-15", has_heartrate: true, average_heartrate: 145, max_heartrate: 172 },
+    { id: "13", name: "Workout", start_date: "2019-03-15", has_heartrate: true, average_heartrate: 145, max_heartrate: 172 },
+    { id: "14", name: "Workout", start_date: "2019-03-15", has_heartrate: true, average_heartrate: 145, max_heartrate: 172 },
+]
 class App extends Component {
 
     constructor() {
         super();
-        const activities = [
-            { id: "0", name: "Warmup Stretch", start_date: "2020-03-14", has_heartrate: false },
-            { id: "1", name: "Run", start_date: "2020-03-14", has_heartrate: true, average_heartrate: 165, max_heartrate: 180 },
-            { id: "2", name: "Yoga", start_date: "2021-03-15", has_heartrate: true, average_heartrate: 60, max_heartrate: 90 },
-            { id: "3", name: "Workout", start_date: "2021-03-15", has_heartrate: true, average_heartrate: 145, max_heartrate: 172 },
-            { id: "4", name: "Workout", start_date: "2021-03-15", has_heartrate: true, average_heartrate: 145, max_heartrate: 172 },
-            { id: "5", name: "Workout", start_date: "2021-03-15", has_heartrate: true, average_heartrate: 145, max_heartrate: 172 },
-            { id: "6", name: "Workout", start_date: "2019-03-15", has_heartrate: true, average_heartrate: 145, max_heartrate: 172 },
-            { id: "7", name: "Workout", start_date: "2019-03-15", has_heartrate: true, average_heartrate: 145, max_heartrate: 172 },
-            { id: "8", name: "Workout", start_date: "2019-03-15", has_heartrate: true, average_heartrate: 145, max_heartrate: 172 },
-            { id: "9", name: "Workout", start_date: "2019-03-15", has_heartrate: true, average_heartrate: 145, max_heartrate: 172 },
-            { id: "10", name: "Workout", start_date: "2019-03-15", has_heartrate: true, average_heartrate: 145, max_heartrate: 172 },
-            { id: "11", name: "Workout", start_date: "2019-03-15", has_heartrate: true, average_heartrate: 145, max_heartrate: 172 },
-            { id: "12", name: "Workout", start_date: "2019-03-15", has_heartrate: true, average_heartrate: 145, max_heartrate: 172 },
-            { id: "13", name: "Workout", start_date: "2019-03-15", has_heartrate: true, average_heartrate: 145, max_heartrate: 172 },
-            { id: "14", name: "Workout", start_date: "2019-03-15", has_heartrate: true, average_heartrate: 145, max_heartrate: 172 },
-        ]
+        const defaultYear = 2021
+        const filteredActivities = activities.filter(e => parseInt(e.start_date.slice(0, 4)) === defaultYear)
         this.state = {
-            selectedActivity: activities[3],
-            selectedYear: new Date().getFullYear(),
-            activityList: activities
+            selectedActivity: filteredActivities[0],
+            selectedYear: defaultYear,
+            activityList: filteredActivities
         };
         this.setProps = this.setProps.bind(this);
     }
 
     setProps(newProps) {
-        this.setState(newProps);
+        if (newProps.selectedYear) {
+            // simulate side effects out of scope of this component
+            const filteredActivities = activities.filter(e => parseInt(e.start_date.slice(0, 4)) === newProps.selectedYear)
+            this.setState({...newProps, activityList: filteredActivities});
+        } else {
+            this.setState(newProps);
+        }
     }
 
     render() {
         return (
-            <div>
+            <div className="app">
                 <ActivitySelector
                     setProps={this.setProps}
                     selectedActivity={this.state.selectedActivity}
                     selectedYear={this.state.selectedYear}
-                    activityList={this.state.activityList.filter(e => parseInt(e.start_date.slice(0, 4)) === this.state.selectedYear)}
-                // {...this.state}
+                    activityList={this.state.activityList}
                 />
-                {this.state.value}
+                <pre className="showState">
+{`Selected Year: ${this.state.selectedYear}
+Selected Activity: ${JSON.stringify(this.state.selectedActivity, null, 2)}
+Activity list: ${JSON.stringify(this.state.activityList, null, 2)}
+                    `}
+                </pre>
             </div>
         )
     }
--- activity_selector/src/lib/components/ActivitySelector.react.js
+++ activity_selector/src/lib/components/ActivitySelector.react.js
@@ -2,12 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import "./ActivitySelector.style.css"
 
+const CURRENT_YEAR = new Date().getFullYear()
+
 /**
- * ExampleComponent is an example component.
- * It takes a property, `label`, and
- * displays it.
- * It renders an input with the property `value`
- * which is editable by the user.
+ * ActivitySelector lets the user select 
+ * the activity from the list of activities given.
+ * Has the option to change the year, 
+ * that triggers the list to change 
+ * out of the scope of this component.
  */
 const ActivitySelector = (props) => {
     const {
@@ -19,35 +21,35 @@ const ActivitySelector = (props) => {
     } = props;
 
     /*
-        * Send the new value to the parent component.
-        * setProps is a prop that is automatically supplied
-        * by dash's front-end ("dash-renderer").
-        * In a Dash app, this will update the component's
-        * props and send the data back to the Python Dash
-        * app server if a callback uses the modified prop as
-        * Input or State.
-        */
-    // e => setProps({ value: e.target.value })
+    * Send the new value to the parent component.
+    * setProps is a prop that is automatically supplied
+    * by dash's front-end ("dash-renderer").
+    * In a Dash app, this will update the component's
+    * props and send the data back to the Python Dash
+    * app server if a callback uses the modified prop as
+    * Input or State.
+    * `e => setProps({ value: e.target.value })`
+    */
     return (
         <div id={id} className="activity-selector-root">
-            <div className="yearSelector">
+            <div className="year-selector">
                 <button
-                    className="prev"
+                    className="button prev"
                     // currently can not use function updater, just object
                     onClick={() => setProps({ selectedYear: selectedYear - 1 })}
                 >
                     {selectedYear - 1}
                 </button>
-                <span>{selectedYear}</span>
+                <span className="current">{selectedYear}</span>
                 <button
-                    className="next"
+                    disabled={CURRENT_YEAR <= selectedYear}
+                    className="button next"
                     onClick={() => setProps({ selectedYear: selectedYear + 1 })}
                 >
                     {selectedYear + 1}
                 </button>
             </div>
             <div className="activity-selector" onClick={(e) => {
-                // console.log(e, e.target.getAttribute("data-value"))
                 const newActivityId = e.target.getAttribute("data-value")
                 const newActivity = activityList.filter(e => ("" + e.id) === newActivityId)[0]
                 if (newActivityId && newActivity) {
